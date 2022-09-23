e988906007922a7b4a91474adeead4bd82218fa5
Jane Fernanda Martins <jfermartins@hotmail.com>
2022-09-23 11:17:15 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/br/com/accenture/course/resources/UserResource.java
+++ src/main/java/br/com/accenture/course/resources/UserResource.java
@@ -1,8 +1,7 @@
 package br.com.accenture.course.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.accenture.course.domain.User;
+import br.com.accenture.course.dto.UserDTO;
 import br.com.accenture.course.services.UserService;
 
 @RestController
@@ -21,11 +21,12 @@ public class UserResource {
 	private UserService service;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		User jane = new User("1", "Jane", "jfermartins@gmail.com");
 		User fernanda = new User("2", "Fernanda", "fernanda@gmail.com");
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
