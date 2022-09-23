279eece55ff202645c6ee1457b0587c384ebe852
Jane Fernanda Martins <jfermartins@hotmail.com>
2022-09-23 11:54:54 -0300

    Obtendo um usuário por id

--- src/main/java/br/com/accenture/course/services/UserService.java
+++ src/main/java/br/com/accenture/course/services/UserService.java
@@ -1,21 +1,28 @@
 package br.com.accenture.course.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.accenture.course.domain.User;
 import br.com.accenture.course.repositories.UserRepository;
+import br.com.accenture.course.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
